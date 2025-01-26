9cc77a8aeae22d9eacc99d631a33d946ee60eb78
Shahd05s <adsshahd8@gmail.com>
2025-01-26 16:59:44 +0000

    Update EuclideanAlgorithm.py
    accept input numbers and make sure its valid (positive integer)

--- EuclideanAlgorithm.py
+++ EuclideanAlgorithm.py
@@ -15,13 +15,30 @@ def find_GCD(a, b):
         rem = a % b # Find the remainder equation 
         a=b # set a equal to the value of b 
         b= rem # set b equal to the remainder
-    return a # Return the last non-zero value of (a)) as the GCD
+    return a # Return the last non-zero value of (a) as the GCD
       
-a=18
-b=9
 
-#apply a, b to the function and print the result (gcd) 
-gcd = find_GCD(a, b)
-print(f"The GCD of {a} and {b} is {gcd}.")
+
+try: # using try-except to deal with error if the user inputed somthing that is not a positive integer 
+# Get integer input from the user 
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+
+    # Check if inputs are valid, positive
+    if num1 > 0 and num2 > 0:
+    # Calculate the GCD
+        print(f"The GCD of {num1} and {num2} is: {find_GCD(num1, num2)}")
+        
+    else: # if the entered number was negative then asks user to a positive number 
+        print(" Please enter positive integers only.")
+        
+except ValueError: #print an error message if the user input is invalid 
+        print(" Invalid input. Please enter integers only.")
+            
+
+
+
+
+
 
 
